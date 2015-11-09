5ace9f0f87fe1c5e4f2532d52b84f35a255e89ea
Steven Havko <stevenhavko@gmail.com>
2015-11-09 16:38:24 -0500

    Creating Views

--- playertracker3/tracker/views.py
+++ playertracker3/tracker/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Player
+from django.template import RequestContext, loader
 # Create your views here.
+
+def index(request):
+	player_list = Player.objects.all()
+	template = loader.get_template('tracker/index.html')
+	context = RequestContext(request, {
+		'player_list': player_list,
+	})
+	
+	return HttpResponse(template.render(context))
+	
+def player(request, player_id):
+	response = "Player: %s"
+	return HttpResponse(response % player_id)
+
