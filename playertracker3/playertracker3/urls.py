43c7f6aab658ddedcccde37419d3d542eb02090c
Steven Havko <stevenhavko@gmail.com>
2015-11-14 18:37:51 -0500

    Changes

--- playertracker3/playertracker3/urls.py
+++ playertracker3/playertracker3/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^tracker/', include('tracker.urls')),
+	url(r'^tracker/', include('tracker.urls', namespace="tracker")),
     url(r'^admin/', include(admin.site.urls))
 ]
